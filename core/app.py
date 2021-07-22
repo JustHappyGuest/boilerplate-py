77e5deabfc42e7da0ec5ccc20e703a408c4e0f2a
Tema Agaev <avagaev@domclick.ru>
2021-07-22 17:15:56 +0300

    Удаление свагера

--- core/app.py
+++ core/app.py
@@ -8,7 +8,6 @@ from flask_apispec.extension import FlaskApiSpec
 from core.register_resources import register_resources
 from core.utils.init_db import init_db
 from core.utils.create_jwt import create_jwt
-from core.register_docs import register_docs
 
 
 APP_TITLE = os.environ.get("APP_TITLE")
@@ -19,19 +18,5 @@ def create_app():
     init_db()
     create_jwt(app)
     register_resources(Api(app))
-
-
-    app.config.update({
-        'APISPEC_SPEC': APISpec(
-            title=APP_TITLE,
-            version='v1',
-            plugins=[MarshmallowPlugin()],
-            openapi_version='2.0.0'
-        ),
-        'APISPEC_SWAGGER_URL': '/swagger-json/',
-        'APISPEC_SWAGGER_UI_URL': '/swagger/'
-    })
-
-    register_docs(FlaskApiSpec(app))
 
     return app
--- task/task_resource.py
+++ task/task_resource.py
@@ -1,27 +1,15 @@
 from flask import jsonify
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required
-from marshmallow import Schema, fields
-from flask_apispec import marshal_with, doc, use_kwargs
-from flask_apispec.views import MethodResource
 
 from task.task_service import create_task, get_tasks
 
 
-class TaskListResource(MethodResource, Resource):
+class TaskListResource(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('name', type=str, required=True, help='This field cannot be left blank')
 
-    class TaskCreate(Schema):
-        name = fields.String(required=True, description="Название задачи")
-
-    class Task(Schema):
-        name = fields.String(required=True, description="Название задачи")
-
     @jwt_required()
-    @doc(description='Добавление задачи', tags=['tasks'])
-    @use_kwargs(TaskCreate, location=('json'))
-    @marshal_with(Task, 200)
     def post(self):
         data = self.parser.parse_args()
 
@@ -35,8 +23,6 @@ class TaskListResource(MethodResource, Resource):
         return jsonify(task)
 
     @jwt_required()
-    @doc(description='Список задач', tags=['tasks'])
-    @marshal_with(Task, 200)
     def get(self):
         error, tasks = get_tasks()
 
@@ -48,6 +34,3 @@ class TaskListResource(MethodResource, Resource):
 
 def register_task_resources(api):
     api.add_resource(TaskListResource, '/tasks/')
-
-def register_task_docs(docs):
-    docs.register(TaskListResource)
