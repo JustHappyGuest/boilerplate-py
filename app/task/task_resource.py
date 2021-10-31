07b7eed2b4ce7c3b37a66a3b8465325c40c5656b
KareFour <ya.aars123@yandex.ru>
2021-10-31 22:45:38 +0300

    Добавлена ручка обновления для task

--- app/task/task_resource.py
+++ app/task/task_resource.py
@@ -1,8 +1,10 @@
+from copy import error
 from flask import jsonify
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required, current_user
+import jwt
 from .task_model import TaskModel
-from .task_service import create_task, get_tasks, delete_task
+from .task_service import create_task, get_tasks, delete_task, update_task
 
 
 class TaskListResource(Resource):
@@ -32,6 +34,9 @@ class TaskListResource(Resource):
         return jsonify(tasks)
 
 class TaskResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str, required=True, help='This field cannot be left blank')
+
     @jwt_required()
     def delete(self, task_id):
         
@@ -42,6 +47,19 @@ class TaskResource(Resource):
 
         return jsonify({'task_id': task_id})
 
+    @jwt_required()
+    def patch(self, task_id):
+
+        data = self.parser.parse_args()
+
+        name = data['name']
+        
+        error, task = update_task(current_user, task_id, name)
+
+        if error:
+            return error
+        
+        return jsonify(task)
 
 def register_task_resources(api):
     api.add_resource(TaskListResource, '/tasks/')
--- app/task/task_service.py
+++ app/task/task_service.py
@@ -1,3 +1,4 @@
+from re import T
 from playhouse.shortcuts import model_to_dict
 
 from app.core.utils.create_error import create_error
@@ -29,6 +30,20 @@ def get_tasks(current_user):
 def delete_task(current_user, task_id):
     try:
         task = TaskModel.get(TaskModel.user_id == current_user.id, TaskModel.id == task_id)
+
         task.delete_instance()
+    except NameError:
+        return create_error(500, NameError), None
+
+
+def update_task(current_user, task_id, new_name):
+    try:
+        task = TaskModel.select()\
+            .where(TaskModel.user_id == current_user.id, TaskModel.id == task_id).get()
+        
+        task.name = new_name
+        task.save()
+
+        return None, {"name": task.name, "id": task.id}
     except NameError:
         return create_error(500, NameError), None
